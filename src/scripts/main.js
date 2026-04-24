5e6c9813756d843fb4c551843802a5a1e71b3226
unknown <nichegolichnogo1717@gmail.com>
2026-04-25 01:33:49 +0300

    add tic tac toe board

---
+++ src/scripts/main.js
@@ -0,0 +1,60 @@
+import '../scss/main.scss';
+const field = document.querySelectorAll('.field');
+
+function createNull(field) {
+  const nulik = document.createElement('div');
+  nulik.style.width = '50px';
+  nulik.style.height = '50px';
+  nulik.style.borderRadius = '50%';
+  nulik.style.border = '3px solid black';
+  field.appendChild(nulik);
+}
+
+function checkField(field) {
+  if (field.children.length === 0) {
+    return true;
+  } else {
+    alert('Больше в одно поле ставить нельзя');
+    return false;
+  }
+}
+
+field.forEach((field) => {
+  field.addEventListener('click', function () {
+    if (this.textContent === '') {
+      if (checkField(field)) {
+        // createNull(this);
+        createCross(this);
+      }
+    }
+  });
+});
+
+function createCross(field) {
+  const cross = document.createElement('div');
+
+  cross.style.position = 'relative';
+  cross.style.width = '50px';
+  cross.style.height = '50px';
+
+  const line1 = document.createElement('div');
+  const line2 = document.createElement('div');
+
+  [line1, line2].forEach((line) => {
+    line.style.position = 'absolute';
+    line.style.width = '100%';
+    line.style.height = '4px';
+    line.style.background = 'black';
+    line.style.top = '50%';
+    line.style.left = '0';
+  });
+
+  line1.style.transform = 'rotate(45deg)';
+  line2.style.transform = 'rotate(-45deg)';
+
+  cross.appendChild(line1);
+  cross.appendChild(line2);
+
+  field.appendChild(cross);
+  field.appendChild(cross);
+}
